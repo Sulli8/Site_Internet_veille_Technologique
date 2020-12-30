96ab4e616b87351c5e1aa809dcf204896b41d016
Sullivan <sulivan.sextius@gmail.om>
2020-12-31 00:24:40 +0100

    + Ajout du design IHM

--- effet.js
+++ effet.js
@@ -1,14 +1,18 @@
-const title = document.getElementById('autotext');
-const text = "Bienvenue sur le Portofolio de Sullivan SEXTIUS ";
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-let index = 0;
-
-const play = () => {
-  title.innerHTML = text.slice(0,index);
-  index++;
-  if(index > text.length){
-    index = 0;
-  }
-};
-let timer = setInterval(play, 300);
-play()
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
